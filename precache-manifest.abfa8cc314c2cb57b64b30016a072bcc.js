4fb30abf8063381fa78df37e5d32a98e2b542b2e
Ionut Panait <pacalici@gmail.com>
2020-04-14 16:02:50 +0300

    Updates

---
+++ precache-manifest.abfa8cc314c2cb57b64b30016a072bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625f32fb1f8801d474ee391ced6d8f2d",
+    "url": "/feedbuilder/index.html"
+  },
+  {
+    "revision": "d80f6c59994da296cd97",
+    "url": "/feedbuilder/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5df70d6905ff5e3d7267",
+    "url": "/feedbuilder/static/css/main.d73fd3b8.chunk.css"
+  },
+  {
+    "revision": "d80f6c59994da296cd97",
+    "url": "/feedbuilder/static/js/2.93a16fc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feedbuilder/static/js/2.93a16fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df70d6905ff5e3d7267",
+    "url": "/feedbuilder/static/js/main.119fce26.chunk.js"
+  },
+  {
+    "revision": "17bf88b92e6377d14784",
+    "url": "/feedbuilder/static/js/runtime-main.f5f405e8.js"
+  }
+]);
