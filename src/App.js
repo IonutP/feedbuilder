41a5526af0a04b946e0a85a0093ec09b0546cd8f
Ionut Panait <pacalici@gmail.com>
2020-04-15 15:57:36 +0300

    removing default values

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         super();
 
         this.state = {
-            siteName: 'classic',
+            siteName: '',
             apiKey: '',
             apiValid: false,
             exchange: '',
