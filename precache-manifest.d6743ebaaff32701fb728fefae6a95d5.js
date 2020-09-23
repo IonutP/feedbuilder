d6344c0727119da7e7e73aed7c4a786ff3d74e83
Ionut Panait <pacalici@gmail.com>
2020-09-23 12:59:25 +0300

    Updates

--- precache-manifest.d6743ebaaff32701fb728fefae6a95d5.js
+++ precache-manifest.d6743ebaaff32701fb728fefae6a95d5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "75ca50967bf168fd82145346cd28bd39",
+    "revision": "9cb45bbed64a629c4a96a935a3d12617",
     "url": "/feedbuilder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/feedbuilder/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "7d172b2b2a3b2720fbe2",
+    "revision": "25e36e8b4dd0b18cafe1",
     "url": "/feedbuilder/static/css/main.3a187e16.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/feedbuilder/static/js/2.573deaff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7d172b2b2a3b2720fbe2",
-    "url": "/feedbuilder/static/js/main.c0db326f.chunk.js"
+    "revision": "25e36e8b4dd0b18cafe1",
+    "url": "/feedbuilder/static/js/main.4adc469f.chunk.js"
   },
   {
     "revision": "17bf88b92e6377d14784",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/feedbuilder/precache-manifest.a4083a0f7ef59e80bd9cb02d10775b1e.js"
+  "/feedbuilder/precache-manifest.d6743ebaaff32701fb728fefae6a95d5.js"
 );
 
 self.addEventListener('message', (event) => {
